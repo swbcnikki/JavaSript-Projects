b29816c5b6c5bd5d460d094a9e594d2c1f609a64
swbcnikki <99786405+swbcnikki@users.noreply.github.com>
2022-03-12 18:34:07 -0800

    js project 3

---
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -0,0 +1,19 @@
+function doingMath() {
+    let res = 4 + 3;
+    document.getElementById("Math").innerHTML = res;
+}
+
+function subtraction_action() {
+    var Subtraction = 22 - 20;
+    document.getElementById("Math").innerHTML = "22-20=" + Subtraction;
+}
+
+function multiplication_station() {
+    var simple_Math = 7 * 10;
+    document.getElementById("Math").innerHTML = "7 * 10 =" + simple_Math;
+}
+
+function division_conquer() {
+    var pi4u = 22 / 7;
+    document.getElementById("Math").innerHTML = "22 / 7 =" + pi4u;
+}
