adafb7737c18f7d6a213256f807f9921fa87f55e
swbcnikki <99786405+swbcnikki@users.noreply.github.com>
2022-03-16 12:29:56 -0700

    update

--- Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
+++ Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
@@ -31,12 +31,12 @@ function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("dot");
-    if (n > 9) { slideIndex = 1 }
-    if (n < 1) { slideIndex = 9 }
-    for (i = 0; i < 9; i++) {
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    for (i = 0; i < 9; i++) {
+    for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
     slides[slideIndex - 1].style.display = "block";
