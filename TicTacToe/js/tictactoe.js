a83259aa3bf0f136908fc2bb492e206a9430825e
swbcnikki <99786405+swbcnikki@users.noreply.github.com>
2022-03-19 13:26:03 -0700

    Changes - js file - 35, 36, 68

--- TicTacToe/js/tictactoe.js
+++ TicTacToe/js/tictactoe.js
@@ -32,39 +32,40 @@ function placeXOrO(squareNumber) {
         } else {
             // Change the activePlayer to 'X'
             activePlayer = 'X';
-            // This function  plays placement sound.
-            Audio('./media/place.mp3');
-            // This condition checks to see if it is computer's turn.
-            if (activePlayer === 'O') {
-                // This function disables clicking for computer choice.
-                disableClick();
-                // This function waits 1 second before computer places image and enables click.
-                setTimeout(function() { computersTurn(); }, 1000)
-            }
-            //Returning true is needed for our computersTurn() function to work.
-            return true;
         }
-        // This function results in a random square being selected.
-        function computersTurn() {
-            //This boolean is needed for our while loop.
-            let success = false;
-            // This variable stores a random number 0-8.
-            let pickASquare;
-            // This condition allows our while loop to keep trying if a square is selected already.
-            while (!success) {
-                // A random number between 0 and 8 is selected.
-                pickASquare = String(Math.floor(Math.random() * 9));
-                // If the random number evaluated returns true, the square hasn't been selected yet.
-                if (placeXOrO(pickASquare)) {
-                    // This line calls the function.
-                    placeXOrO(pickASquare);
-                    // This changes our boolean and ends the loop.
-                    success = true;
-                };
-            }
+        // This function  plays placement sound.
+        audio('./media/place.mp3');
+        // This condition checks to see if it is computer's turn.
+        if (activePlayer === 'O') {
+            // This function disables clicking for computer choice.
+            disableClick();
+            // This function waits 1 second before computer places image and enables click.
+            setTimeout(function() { computersTurn(); }, 1000)
+        }
+        //Returning true is needed for our computersTurn() function to work.
+        return true;
+    }
+    // This function results in a random square being selected.
+    function computersTurn() {
+        //This boolean is needed for our while loop.
+        let success = false;
+        // This variable stores a random number 0-8.
+        let pickASquare;
+        // This condition allows our while loop to keep trying if a square is selected already.
+        while (!success) {
+            // A random number between 0 and 8 is selected.
+            pickASquare = String(Math.floor(Math.random() * 9));
+            // If the random number evaluated returns true, the square hasn't been selected yet.
+            if (placeXOrO(pickASquare)) {
+                // This line calls the function.
+                placeXOrO(pickASquare);
+                // This changes our boolean and ends the loop.
+                success = true;
+            };
         }
     }
 }
+
 
 // This function parses the selectedSquares array to search for win conditions.
 // drawWinLine function is called to draw a line if condition is met.
